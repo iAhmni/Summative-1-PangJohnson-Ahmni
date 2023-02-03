413fe553ba00cb289afe53090813029cbe1d6a4f
ikpaetukgabriel <gabrielikpaetuk@byui.edu>
2023-02-03 16:41:53 -0700

    add model and controller package

---
+++ summative-project-one/src/main/java/com/company/summativeprojectone/controllers/QuoteServiceController.java
@@ -0,0 +1,4 @@
+package com.company.summativeprojectone.controllers;
+
+public class QuoteServiceController {
+}
---
+++ summative-project-one/src/main/java/com/company/summativeprojectone/models/Quote.java
@@ -0,0 +1,4 @@
+package com.company.summativeprojectone.models;
+
+public class Quote {
+}
