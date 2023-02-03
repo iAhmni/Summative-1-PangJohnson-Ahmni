c2ebe7d1eeee95e5245a7024341bf0cf81eda291
Ahmni <ahmni@ku.edu>
2023-02-03 17:28:15 -0600

    spring init

---
+++ summative-project-1/src/main/java/com/company/summativeproject1/SummativeProject1Application.java
@@ -0,0 +1,13 @@
+package com.company.summativeproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummativeProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummativeProject1Application.class, args);
+	}
+
+}
