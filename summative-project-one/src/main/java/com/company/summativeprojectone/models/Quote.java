95dd478f7bb375f2160f4d45026880b425e2b78a
ikpaetukgabriel <gabrielikpaetuk@byui.edu>
2023-02-03 16:47:34 -0700

    add Quote

--- summative-project-one/src/main/java/com/company/summativeprojectone/models/Quote.java
+++ summative-project-one/src/main/java/com/company/summativeprojectone/models/Quote.java
@@ -1,4 +1,36 @@
 package com.company.summativeprojectone.models;
 
 public class Quote {
+    String id;
+    String author;
+    String quote;
+
+    public Quote(String author, String quote) {
+        this.author = author;
+        this.quote = quote;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
 }
