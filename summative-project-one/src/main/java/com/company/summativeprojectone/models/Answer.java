d9b0c022d617029c0c5589620230c22bc0f6a9eb
ikpaetukgabriel <gabrielikpaetuk@byui.edu>
2023-02-03 16:53:59 -0700

    change Quote type to int, add Answer POJO

---
+++ summative-project-one/src/main/java/com/company/summativeprojectone/models/Answer.java
@@ -0,0 +1,36 @@
+package com.company.summativeprojectone.models;
+
+public class Answer {
+    int id;
+    String question;
+    String answer;
+
+    public Answer(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+}
--- summative-project-one/src/main/java/com/company/summativeprojectone/models/Quote.java
+++ summative-project-one/src/main/java/com/company/summativeprojectone/models/Quote.java
@@ -1,7 +1,7 @@
 package com.company.summativeprojectone.models;
 
 public class Quote {
-    String id;
+    int id;
     String author;
     String quote;
 
@@ -10,11 +10,11 @@ public class Quote {
         this.quote = quote;
     }
 
-    public String getId() {
+    public int getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(int id) {
         this.id = id;
     }
 
