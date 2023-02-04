fa7efffdefe044408f7ad92835573e54530d7da3
ikpaetukgabriel <gabrielikpaetuk@byui.edu>
2023-02-03 18:02:09 -0700

    add test for Answer controller

--- summative-project-one/src/test/java/com/company/summativeprojectone/controller/AnswerControllerTest.java
+++ summative-project-one/src/test/java/com/company/summativeprojectone/controller/AnswerControllerTest.java
@@ -1,4 +1,36 @@
-import static org.junit.Assert.*;
+package com.company.summativeprojectone.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(QuoteController.class)
+
 public class AnswerControllerTest {
-  
+    // Wiring in the MockMvc object
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnAQuote() throws Exception {
+        String question = "How are you?";
+        // ACT
+        mockMvc.perform(post("/magic")
+                        .content(question)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$").isNotEmpty());
+    }
 }
