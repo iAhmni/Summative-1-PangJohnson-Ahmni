e93311a58a96d6e97bb70757ed14974ae9ca51cb
ikpaetukgabriel <gabrielikpaetuk@byui.edu>
2023-02-03 17:39:21 -0700

    add test for QuoteController

--- summative-project-one/src/main/java/com/company/summativeprojectone/controller/QuoteController.java
+++ summative-project-one/src/main/java/com/company/summativeprojectone/controller/QuoteController.java
@@ -2,7 +2,10 @@ package com.company.summativeprojectone.controller;
 
 import com.company.summativeprojectone.models.Quote;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,16 +17,16 @@ public class QuoteController {
     private Random randomGenerator = new Random();
 
     public QuoteController() {
-        this.quotes.add(new Quote(1,"Nelson Mandela", "The greatest glory in living lies not in never falling, but in rising every time we fall."));
-        this.quotes.add(new Quote(2,"Walt Disney", "The way to get started is to quit talking and begin doing."));
-        this.quotes.add(new Quote(3,"Steve Jobs", "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking."));
-        this.quotes.add(new Quote(4,"Eleanor Roosevelt", "If life were predictable it would cease to be life, and be without flavor."));
-        this.quotes.add(new Quote(5,"Oprah Winfrey", "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough."));
-        this.quotes.add(new Quote(6,"James Cameron", "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success."));
-        this.quotes.add(new Quote(7,"John Lennon", "Life is what happens when you're busy making other plans."));
-        this.quotes.add(new Quote(8,"Cristiano Ronaldo", "We cannot live being obsessed with what other people think about us. It's impossible to live like that. Not even God managed to please the entire world."));
-        this.quotes.add(new Quote(9,"Anne Frank", "Whoever is happy will make others happy too. "));
-        this.quotes.add(new Quote(10,"Ralph Waldo Emerson", "Do not go where the path may lead, go instead where there is no path and leave a trail."));
+        this.quotes.add(new Quote(1, "Nelson Mandela", "The greatest glory in living lies not in never falling, but in rising every time we fall."));
+        this.quotes.add(new Quote(2, "Walt Disney", "The way to get started is to quit talking and begin doing."));
+        this.quotes.add(new Quote(3, "Steve Jobs", "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking."));
+        this.quotes.add(new Quote(4, "Eleanor Roosevelt", "If life were predictable it would cease to be life, and be without flavor."));
+        this.quotes.add(new Quote(5, "Oprah Winfrey", "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough."));
+        this.quotes.add(new Quote(6, "James Cameron", "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success."));
+        this.quotes.add(new Quote(7, "John Lennon", "Life is what happens when you're busy making other plans."));
+        this.quotes.add(new Quote(8, "Cristiano Ronaldo", "We cannot live being obsessed with what other people think about us. It's impossible to live like that. Not even God managed to please the entire world."));
+        this.quotes.add(new Quote(9, "Anne Frank", "Whoever is happy will make others happy too. "));
+        this.quotes.add(new Quote(10, "Ralph Waldo Emerson", "Do not go where the path may lead, go instead where there is no path and leave a trail."));
     }
 
     @RequestMapping(value = "/quote", method = RequestMethod.GET)
---
+++ summative-project-one/src/test/java/com/company/summativeprojectone/controller/QuoteControllerTest.java
@@ -0,0 +1,29 @@
+package com.company.summativeprojectone.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(QuoteController.class)
+public class QuoteControllerTest {
+
+    // Wiring in the MockMvc object
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnAQuote() throws Exception {
+        // ACT
+        mockMvc.perform(get("/quote"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+}
